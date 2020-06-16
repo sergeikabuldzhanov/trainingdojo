0c5caa71d603655cda6c4a076981a984ef840c28
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-06-16 21:21:50 +0500

    Subclassing

--- OOP/Subclassing/Classes.js
+++ OOP/Subclassing/Classes.js
@@ -1,10 +1,10 @@
 class User {
-    // constructor function is used the same way as userCreator from prev examples
+  // constructor function is used the same way as userCreator from prev examples
   constructor(name, score) {
     this.name = name;
     this.score = score;
   }
-//   methods are appended to User.prototype
+  //   methods are appended to User.prototype
   sayName() {
     console.log(this.name);
   }
@@ -13,5 +13,23 @@ class User {
   }
 }
 
-const user1 = new User('Bob', 10);
-const user2 = new User('Paul', 20);
+const user1 = new User("Bob", 10);
+const user2 = new User("Paul", 20);
+
+user1.sayName();
+
+class PaidUser extends User {
+  constructor(name, score, balance) {
+    // super calls the parent class constructor function and sets the PaidUser.prototype.__proto__
+    //   to User.prototype
+    super(name, score);
+    this.balance = balance;
+  }
+  increaseBalance() {
+    this.balance++;
+  }
+}
+
+const paid1 = new PaidUser("Alice", 22, 1000);
+
+paid1.increaseBalance();// 1001
