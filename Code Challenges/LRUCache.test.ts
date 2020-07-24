be0337fac7e7ab3509efd714b8307fd05ec580bc
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-07-24 16:43:53 +0500

    LRUCache tests

--- Code Challenges/LRUCache.test.ts
+++ Code Challenges/LRUCache.test.ts
@@ -29,6 +29,10 @@ class LRUCache {
     this.capacity = capacity;
     this.storage = new Map();
   }
+  /**
+   * @remarks returns value associated with that key, if there's no entry with that key returns -1
+   * @param key key for the value to be retrieved
+   */
   get(key: any) {
     if (!this.storage.has(key)) return -1;
     const val = this.storage.get(key);
@@ -36,6 +40,12 @@ class LRUCache {
     this.storage.set(key, val);
     return val;
   }
+  /**
+   * @remarks
+   * Adds key:value pair to the cache, if cache is at max capacity the oldest entry is evicted
+   * @param key: value to be used as key, objects, functions, and all the primitives can be used as keys
+   * @param val: value assigned to that key
+   */
   put(key: any, val: any) {
     if (this.storage.has(key)) {
       this.storage.delete(key);
@@ -48,3 +58,17 @@ class LRUCache {
     this.storage.set(key, val);
   }
 }
+
+describe("LRUCache", () => {
+  test("Adding items from zero to limit", () => {
+    //setup
+    const cache = new LRUCache(2);
+    cache.put(1, "A");
+    expect(cache.get(1)).toBe("A");
+    cache.put(2, "B");
+    expect(cache.get(2)).toBe("B");
+    cache.put(3, "C");
+    expect(cache.get(3)).toBe("C");
+    expect(cache.get(1)).toBe(-1);
+  });
+});
