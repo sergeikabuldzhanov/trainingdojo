740ddd3a32b2d8849a55455bda5316275bc791cd
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-08-02 08:03:34 +0500

    First unique char in a string

---
+++ Code Challenges/FirstUniqueChar.test.ts
@@ -0,0 +1,39 @@
+/* 
+Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode"
+return 2.
+
+*/
+var firstUniqChar = function (s: string): number {
+  const charCount: { [key: string]: number[] } = {};
+  for (let i = 0; i < s.length; i++) {
+    if (charCount[s[i]]) {
+      charCount[s[i]].push(i);
+    } else {
+      charCount[s[i]] = [i];
+    }
+  }
+  const kvPairs = Object.entries(charCount);
+  for (let i = 0; i < kvPairs.length; i++) {
+    if (kvPairs[i][1].length === 1) return kvPairs[i][1][0];
+  }
+  return -1;
+};
+
+describe("First Unique character", () => {
+  test("Case 1", () => {
+    expect(firstUniqChar("leetcode")).toBe(0);
+  });
+  test("Case 2", () => {
+    expect(firstUniqChar("loveleetcode")).toBe(2);
+  });
+  test("No unique chars", () => {
+    expect(firstUniqChar("aabbcc")).toBe(-1);
+  });
+});
