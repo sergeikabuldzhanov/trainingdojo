954e977c22543d6574c2d9ebf61339d6b3cebbc3
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-09-04 11:23:25 +0500

    Typescript version

--- Code Challenges/Robber3.test.ts
+++ Code Challenges/Robber3.test.ts
@@ -15,3 +15,18 @@ class Solution:
         res = self.helper(root);
         return max(res[0], res[1])
 */
+
+function helper(root: TreeNode | null) {
+  const res = [0, 0];
+  if (!root) return res;
+  const left = helper(root.left);
+  const right = helper(root.right);
+  res[0] = root.val + left[1] + right[1];
+  res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+  return res;
+}
+
+function rob(root: TreeNode | null) {
+  if (!root) return 0;
+  return Math.max(helper(root));
+}
